18783900bda9a74d8f970103bc9b72dba414b804
Rodrigo Enju <rodrigo.enju@usp.br>
2018-10-17 20:05:47 -0300

    primeiro commit

---
+++ geocomp/voronoi/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""Algoritmos para o Problema do Fecho Convexo:
+
+Dado um conjunto de pontos, determinar o seu fecho convexo.
+
+Algoritmos disponiveis:
+- Graham
+- Embrulho Para Presente
+- Quick Hull
+- Incremental Probabilistico
+- Merge Hull
+- Um algoritmo otimo proposto por Chan
+- Um algoritmo otimo proposto por Bhattacharya e Sen
+
+algoritmo otimo = executa em tempo O(n lg(h)), n = numero de pontos, 
+                                               h = numero de arestas no fecho
+"""
+from . import fortune
+
+# cada entrada deve ter:
+#  [ 'nome-do-modulo', 'nome-da-funcao', 'nome do algoritmo' ]
+children = [ 
+	[ 'fortune', 'Fortune', 'Fortune' ]
+]
+
+#children = algorithms
+
+#__all__ = [ 'graham', 'gift' ]
+__all__ = [a[0] for a in children]
---
+++ geocomp/voronoi/fortune.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+#from geocomp.common.polygon import Polygon
+#from geocomp.common import control
+#from geocomp.common.guiprim import *
+#from functools import cmp_to_key
+
+def Fortune (l):
+	pass
